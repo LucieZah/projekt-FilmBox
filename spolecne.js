50d6d3a9930052bc1d8c59b6edaac9ed2e41d4b0
Lucie Zahalkova <lucie.zahalkova@protonmail.com>
2024-04-10 13:26:44 +0200

    Tlacitko menu

--- spolecne.js
+++ spolecne.js
@@ -1,14 +1,20 @@
 const menu = document.querySelector("#menu-tlacitko")
 
 const polozky = document.querySelector("#menu-polozky")
+const zobrazeniMenu = document.querySelector (".fas.fa-bars")
 
 menu.addEventListener ("click", event => {
     let jeZobrazeny = false
     if (!polozky.classList.contains('show')) {
     polozky.classList.add ("show")
+    zobrazeniMenu.classList.remove (`fa-bars`)
+    zobrazeniMenu.classList.add (`fa-xmark`)
     jeZobrazeny = true
     } else {
     polozky.classList.remove ("show")
+    zobrazeniMenu.classList.add (`fa-bars`)
+    zobrazeniMenu.classList.remove (`fa-xmark`)
     jeZobrazeny = false
 }})
+
 
